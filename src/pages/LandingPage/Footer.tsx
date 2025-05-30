2905fe9eed6d24a44699531c7a395ca772fa3017
GeneralMarshal <112048946+GeneralMarshal@users.noreply.github.com>
2025-05-30 14:53:37 +0100

    final button classes

--- src/pages/LandingPage/Footer.tsx
+++ src/pages/LandingPage/Footer.tsx
@@ -1,16 +1,18 @@
 import { FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
 
 interface Props{
     showModal: boolean
     setShowModal: React.Dispatch<React.SetStateAction<boolean>>
 }
 export default function Footer({showModal, setShowModal}:Props){
+    const [isHover, setIsHover] = useState<boolean>(false)
     return (
         <footer className=" bg-[#F1F0EC] w-full py-10 px-12">
             <p className=" text-3xl tracking-widest font-[julius]">ENQUIRE NOW</p>
             <p className=" text-lg my-4">Find out how you can be a part of the skyline towers</p>
             <button 
-                className=" flex  border gap-12 py-1.5 px-2 rounded-sm font-[julius] items-center mt-8 mb-[100px] border-[#7C7262] text-[#7C7262]"
+                className=" flex  border gap-12 py-1.5 px-2 w-[150px] rounded-sm font-[julius] items-center mt-8 mb-[100px] border-[#7C7262] text-[#7C7262] cursor-pointer hover:bg-[#7C7262] hover:text-[#ffffff] "
                 onClick={()=>{
                     setShowModal(!showModal)
                     setTimeout(()=>{
@@ -20,8 +22,18 @@ export default function Footer({showModal, setShowModal}:Props){
                         })
                     }, 500)
                 }}
+                onMouseEnter={
+                    ()=>{
+                        setIsHover(true)
+                    }
+                }
+                onMouseLeave={
+                    ()=>{
+                        setIsHover(false)
+                    }
+                }
             >
-                ENQUIRE
+                {isHover ? "ENQUIRE" : "INQUIRE"}
                 <FaArrowRight />
             </button>
         </footer>
--- src/pages/LandingPage/Modal.tsx
+++ src/pages/LandingPage/Modal.tsx
@@ -10,7 +10,7 @@ export default function Modal(){
             <textarea className=" my-2 text-md p-y2 px-2 rounded-sm border border-[#1a1a1a] h-24" name="" id="" placeholder="Your message">
 
             </textarea>
-            <button className=" flex border gap-12 py-1.5 px-2 rounded-sm items-center mt-8 border-[#7C7262] text-[#7C7262] w-[140px] font-[julius]">
+            <button className=" flex border gap-12 py-1.5 px-2 rounded-sm items-center mt-8 border-[#7C7262] text-[#7C7262] w-[140px] font-[julius] hover:bg-[#7C7262] hover:text-[#ffffff] cursor-pointer">
                 SUBMIT
                 <FaArrowRight />
             </button>
